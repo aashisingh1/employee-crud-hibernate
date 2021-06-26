eb7fa7fe077ec806b5d340323d02c9fbf2dea798
aashisingh1 <saashi364@gmail.com>
2021-06-27 03:19:22 +0530

    changes

---
+++ src/main/java/com/springboot/service/ProductService.java
@@ -0,0 +1,17 @@
+package com.springboot.service;
+
+import java.util.List;
+
+import com.springboot.model.Product;
+
+public interface ProductService {
+	Product createProduct(Product product);
+
+	Product updateProduct(Product product);
+
+	List<Product> getAllProduct();
+
+	Product getProductById(long productId);
+
+	void deleteProduct(long id);
+}
